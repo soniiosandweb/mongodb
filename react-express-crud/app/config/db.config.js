3dc7472e87b6627fade9ccd95b7cccb64e15be91
soni <soni@iosandweb.net>
2024-04-19 15:53:07 +0530

    changes new

---
+++ react-express-crud/app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://iaw_user:TOQv8aciZ6YAWNNO@iaw.tuebikh.mongodb.net/"
+};
--- react-express-crud/server.js
+++ react-express-crud/server.js
@@ -16,6 +16,21 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    // console.log(db);
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
