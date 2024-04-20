86a2291d221b79929e6d561b0dc5680845098468
soniiosandweb <soni@iosandweb.net>
2024-04-20 17:50:07 +0530

    react app changes

--- react-express-crud/server.js
+++ react-express-crud/server.js
@@ -33,7 +33,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to application." });
 });
 
 // set port, listen for requests
